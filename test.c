e5f99bbbd68c69a8ccd7a89ccafb0a5b3b741342
Blaise Elie <elieblaise04@gmail.com>
2025-09-25 00:33:19 -0400

    Premier commit

---
+++ test.c
@@ -0,0 +1,7 @@
+//Ce programme est juste un essai pour utiliser Github pour permettre la bonne gestion de mon projet 
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    printf("Ceci est un test pour Github.\n");
+    return 0;
+}
