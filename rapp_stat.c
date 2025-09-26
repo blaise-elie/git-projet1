5f86fb81d9397cb80b6a8635ed9784a6e6bcdba3
Blaise Elie <elieblaise04@gmail.com>
2025-09-26 12:10:07 -0400

    ajouter stat

--- rapp_stat.c
+++ rapp_stat.c
@@ -2,6 +2,8 @@
 /* FCHIER:                          rapp_stat.c                           */
 /*AUTEUR:                           Blaise Elie                           */    
 /*DATE DE CREATIION:                25/09/2025                            */  
-/*DATE DE MODIFICATION:             25/09/2025                            */ 
+/*DATE DE MODIFICATION:             26/09/2025                            */ 
 /*DESCRIPTION:                     Rapport et statistiques des entreprises*/    
-/*------------------------------------------------------------------------*/                                                    
+/*------------------------------------------------------------------------*/  
+
+
