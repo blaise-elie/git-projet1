778e13de8354b3d8e58a2245029ce690a29be24b
Blaise Elie <elieblaise04@gmail.com>
2025-09-27 16:08:59 -0400

    mise a jour de rapport

--- rapp_stat.h
+++ rapp_stat.h
@@ -11,39 +11,30 @@
 
 #ifndef RAPP_STAT_H
 #define RAPP_STAT_H
-// Prototypes des fonctions pour les rapports et statistiques
-void list_produits_dispo(char* id_produit, char* nom_produit, int quantite_stock);
-void list_produits_fabrique(char* id_produit, char* nom_produit, int quantite_fabrique);
-void chiffre_affaire(char* id_produit,char* nom_produit, float prix_unitaire, int quantite_vendue);
-void chiffre_affaire_total();
+#include <stdio.h>
+#include <time.h>
 
-
-//fonction chiffre affaire total est incomplete
-
-
-
-
+//structure pour stocker une periode de date
+typedef struct
+  {
+  struct tm date_debut;
+  struct tm date_fin;
+  }Periode;
 
 
 
 
+//fonction pour les listes des produits disponibles
 
+void list_produits_dispo(void);
 
+//fonction pour les listes de produits fabriquers
 
+void list_prod_fab(Periode periode);
 
+//fonction pour les chiffre d'affaire
 
-
-
-
-
-
-
-
-
-
-
-
-
+void chiffre_affaire(Periode periode);
 
 
 
@@ -51,57 +42,3 @@ void chiffre_affaire_total();
 
 
 #endif
-                                    /*----------------------------------------------------------------------------*/
-
-/*FICHIER:                          rapp_stat.h                               */
-/*AUTEUR:                           Blaise Elie                               */   
-/*DATE DE CREATIION:                26/09/2025                               `*/
-/*DATE DE MODIFICATION:             26/09/2025                                */
-/*DESCRIPTION:                      Fichier en tete que contient les prototypes
-                                    des fonctions  rapports et statistiques   */   
-
-/*-------------------------------- -------------------------------------------*/
-
-#ifndef RAPP_STAT_H
-#define RAPP_STAT_H
-// Prototypes des fonctions pour les rapports et statistiques
-void list_produits_dispo(char* id_produit, char* nom_produit, int quantite_stock);
-void list_produits_fabrique(char* id_produit, char* nom_produit, int quantite_fabrique);
-void chiffre_affaire(char* id_produit,char* nom_produit, float prix_unitaire, int quantite_vendue);
-void chiffre_affaire_total();
-
-
-//fonction chiffre affaire total est incomplete
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
-                                    
