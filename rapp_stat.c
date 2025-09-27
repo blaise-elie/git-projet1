9e22e4c3e07445e9c5c9ff16b5eca65575c33461
Blaise Elie <elieblaise04@gmail.com>
2025-09-27 16:20:41 -0400

    Mise a jour de rapport.c

--- rapp_stat.c
+++ rapp_stat.c
@@ -8,3 +8,26 @@
 
 #include <stdio.h>
 #include "rapp_stat.h"
+
+//Implementation des fonctions declarer dans rapp_stat.h
+
+//fonction pour les listes des produits disponibles qui donnent la liste des produits disponibles en stock repartie par usine
+//et par produit elle doit afficher le nom de l'usine, le nom du produit et la quantite disponible
+void list_produits_dispo(void)
+{
+    printf("Liste des produits disponibles en stock :\n");
+    // Code pour lister les produits disponibles
+}
+
+//fonction pour les listes de produits fabriquers
+void list_prod_fab(Periode periode)
+{
+    printf("Liste des produits fabriquers :\n");
+    // Code pour lister les produits fabriquers
+}
+//fonction pour les chiffre d'affaire
+void chiffre_affaire(Periode periode)
+{
+    printf("Chiffre d'affaire :\n");
+    // Code pour calculer le chiffre d'affaire
+}
